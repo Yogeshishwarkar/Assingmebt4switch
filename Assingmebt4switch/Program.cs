a21d5601f492f88b0f913f4269496f6cf700e633
SM <SM@SM-PC>
2022-04-07 11:24:49 +0530

    Add project files.

---
+++ Assingmebt4switch/Program.cs
@@ -0,0 +1,28 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Enter the operator you choose");
+char opert = Convert.ToChar(Console.ReadLine());
+Console.WriteLine("Enter 1st number");
+int num1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Enter 2nd number");
+int num2 = Convert.ToInt32(Console.ReadLine());
+int result;
+switch(opert)
+{
+    case '+':
+        result = num1 + num2;
+        Console.WriteLine("Sum of two number is="+result);
+        break;
+    case '-':
+        result = num1 - num2;
+        Console.WriteLine("Subsraction of two number is ="+result);
+        break;
+    case '*':
+        result = num1 * num2;
+        Console.WriteLine("Multiplication of two number is =" + result);
+        break;
+    case '/':
+        result = num1 / num2;
+        Console.WriteLine("Division of two number is =" + result);
+        break;
+
+}
